43bda6c427097ee126b7c531ba350d7b30dc3cfd
Luca Muratore <luca.muratore@iit.it>
2016-09-13 12:24:02 +0200

    - changed sensePosition to senseMotorPosition

--- src/drc_poses_thread.cpp
+++ src/drc_poses_thread.cpp
@@ -292,7 +292,7 @@ yarp::sig::Vector drc_poses_thread::joint_sense()
 {
     yarp::sig::Vector out(kinematic_joints);
     
-    yarp::sig::Vector in = robot.sensePosition(); //REAL_ROBOT
+    yarp::sig::Vector in = robot.senseMotorPosition(); //REAL_ROBOT
 //     yarp::sig::Vector in = robot.sensePosition(); //SIMULATION
 
     for(int i=0;i<out.size();i++) out[i]=in[i];
